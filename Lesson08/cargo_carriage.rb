eb98fdb9178dfa5b73a86af2eebc395e0098c5fd
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-10-21 18:57:11 +0300

    Add cargo_carriage class with specific methods to work with volume

---
+++ Lesson08/cargo_carriage.rb
@@ -0,0 +1,19 @@
+class CargoCarriage < Carriage
+  attr_reader :volume
+  attr_reader :available_volume
+  attr_reader :taken_volume
+
+  def initialize(volume)
+    @volume = volume
+    @available_volume = volume
+    @taken_volume = 0
+    super(:cargo)
+  end
+
+  def take_volume(volume_to_take)
+    if volume_to_take <= @available_volume
+      @available_volume -= volume_to_take
+      @taken_volume += volume_to_take
+    end
+  end
+end

e86cce85456b5494c4a8ebbcbc2c2b8cbff83a42
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-10-21 19:02:29 +0300

    Add PassengerCarriage class with specific methods to work with seats

---
+++ Lesson08/passenger_carriage.rb
@@ -0,0 +1,19 @@
+class PassengerCarriage < Carriage
+  attr_reader :capacity
+  attr_reader :available_seats
+  attr_reader :taken_seats
+
+  def initialize(capacity)
+    @capacity = capacity
+    @available_seats = capacity
+    @taken_seats = 0
+    super(:passenger)
+  end
+
+  def take_a_seat
+    if @available_seats > 0
+      @available_seats -= 1
+      @taken_seats += 1
+    end
+  end
+end

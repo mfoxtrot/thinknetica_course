5b78b443242854a052f98d7d5f2f01f1add876dc
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-10-22 17:36:05 +0300

    CargoTrain class tested by Rubocop

--- Lesson09/cargo_train.rb
+++ Lesson09/cargo_train.rb
@@ -1,10 +1,16 @@
+# CargoTrain class having specific show_carriage_info method
 class CargoTrain < Train
-
   def type
     :cargo
   end
 
   def show_carriage_info
-    self.each_carriage {|c, number| puts "Carriage: #{number}. Total volume: #{c.volume}, available volume: #{c.available_volume}, taken volume: #{c.taken_volume}"}
+    each_carriage do |c, number|
+      info ||= "Carriage: #{number}. "
+      info << "Total volume: #{c.volume}, "
+      info << "available volume: #{c.available_volume}, "
+      info << "taken volume: #{c.taken_volume}"
+      puts info
+    end
   end
 end

20851cbd32781cbb4228ad81ee80770edeb9f633
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-10-30 16:03:42 +0300

    Rubocop test

--- Lesson10/test.rb
+++ Lesson10/test.rb
@@ -11,18 +11,17 @@ class Test
   validate :a, :type, Integer
 end
 
-
 t = Test.new
 puts "a_history = #{t.a_history}"
 t.a = 10
 t.a = 20
 puts t.a
 puts "a_history = #{t.a_history}"
-puts "==================="
-t.s = "123"
+puts '==================='
+t.s = '123'
 puts "s=#{t.s}"
 
-t=Test.new
-t.a="100"
+t = Test.new
+t.a = '100'
 puts t.a
 puts t.valid?
--- Lesson10/validation.rb
+++ Lesson10/validation.rb
@@ -1,13 +1,20 @@
 module Validation
-  @@validation_types = { presence: 'validate_presence!', format: 'validate_format!',type: 'validate_type!' }
+  @@validation_types = {
+    presence: 'validate_presence!',
+    format: 'validate_format!',
+    type: 'validate_type!'
+  }
 
   @@to_validate = {}
 
   def validate(var_name, validation_type, *args)
     @@to_validate[var_name] ||= []
-    @@to_validate[var_name] << { method: @@validation_types[validation_type], args: args }
+    @@to_validate[var_name] << {
+      method: @@validation_types[validation_type],
+      args: args
+    }
 
-    define_method("validate!") do
+    define_method('validate!') do
       @@to_validate.each do |key, value|
         value.each do |validation_params|
           method_name = validation_params[:method]
@@ -18,24 +25,24 @@ module Validation
       true
     end
 
-    define_method("validate_presence!") do |var|
+    define_method('validate_presence!') do |var|
       raise "Variable #{var_name} is nil" if var.nil?
     end
 
-    define_method("validate_format!") do |var, format_string|
+    define_method('validate_format!') do |var, format_string|
       message = "Variable #{var_name} hasn't exceptable format #{format_string}"
       raise message unless var =~ format_string
     end
 
-    define_method("validate_type!") do |var, checking_class|
-      message = "Variable #{var_name} has type that doesn't matter #{checking_class}"
+    define_method('validate_type!') do |var, checking_class|
+      message = "Var #{var_name} has wrond class. Right is #{checking_class}"
       raise message unless var.class == checking_class
     end
 
-    define_method("valid?") do
+    define_method('valid?') do
       begin
-        send("validate!")
-      rescue
+        send('validate!')
+      rescue StandardError
         false
       end
     end
